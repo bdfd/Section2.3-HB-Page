181ee895ede4516b6a9ba5542cb14857d6d234f9
BDFD <bdfd2005@gmail.com>
2021-01-07 15:51:53 -0500

    revise page order

--- public/js/indexl.js
+++ public/js/indexl.js
@@ -24,7 +24,7 @@ var S = {
 		if (i !== -1) {
 			S.UI.simulate(decodeURI(action).substring(i + 3));
 		} else {
-			S.UI.simulate('Hi 猫猫|祝你|生日|快乐||#countdown 3||');
+			S.UI.simulate('祝你|生日|快乐||#countdown 3||');
 		}
 
 		S.Drawing.loop(function () {
